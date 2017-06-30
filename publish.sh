f34fb1a704c3570488864b7312d4e2d313a470f3
David Dulak <daviddulak@me.com>
2017-06-30 16:15:42 -0500

    oops, lets keep the keys secret

--- publish.sh
+++ publish.sh
@@ -2,13 +2,15 @@
 
 # Make sure to brew install s3cmd
 
+source .env
+
 npm run build
 
 dist="dist/"
 
 s3Bucket="vintagecomet.com"
-s3Key="AKIAIIAGFH3GAHCYCHAQ"
-s3Secret="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"
+s3Key=$AWSAccessKeyId
+s3Secret=$AWSSecretKey
 
 s3cmd put -M -r --exclude '*.svg' --access_key=${s3Key} --secret_key=${s3Secret} ${dist} s3://${s3Bucket}/
 s3cmd put -M -r -m 'image/svg+xml' --exclude '*' --include '*.svg' --access_key=${s3Key} --secret_key=${s3Secret} ${dist} s3://${s3Bucket}/
